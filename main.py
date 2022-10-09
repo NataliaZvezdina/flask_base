7e54800fb37da26e37b5a318a6026bf009f278f7
NataliaZvezdina <natalja.zvezdina@yandex.by>
2022-10-10 00:11:11 +0300

    handle login request, 404 error handling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, session, redirect, url_for, abort
 
 
 app = Flask(__name__)
@@ -22,6 +22,29 @@ def contact():
             flash('Error while sending', category='error')
 
     return render_template('contact.html')
+
+
+@app.route('/profile/<username>')
+def profile(username):
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
+    return f'{username} profile'
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == 'POST' and request.form['username'] == 'test' and request.form['password'] == 'qwerty':
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+
+    return render_template('login.html')
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
 
 
 if __name__ == "__main__":
