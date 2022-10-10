8a3eeb40c4ea6473e00b6800571190644f492101
NataliaZvezdina <natalja.zvezdina@yandex.by>
2022-10-10 23:22:29 +0300

    connect db sqlite

--- main.py
+++ main.py
@@ -1,15 +1,48 @@
 import os
+import sqlite3
+from flask import Flask, render_template, request, flash, session, redirect, url_for, abort, g
 
-from flask import Flask, render_template, request, flash, session, redirect, url_for, abort
+DATABASE = '/tmp/fl_base.db'
+DEBUG = True
+SECRET_KEY = os.urandom(10)
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.urandom(10)
+app.config.from_object(__name__)
+
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'fl_base.db')))
+
+
+def connect_db():
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def create_db():
+    db = connect_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+        db.commit()
+        db.close()
+
+
+def get_db():
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    db = get_db()
+    return render_template('index.html', menu=[])
 
 
 @app.route('/contact', methods=['POST', 'GET'])
